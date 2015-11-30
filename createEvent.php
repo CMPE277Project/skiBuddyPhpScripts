cea8f38d2c4804518c39c6647792a180f5a89e16
rjtbansal <rjtbansal@yahoo.com>
2015-11-29 16:51:16 -0800

    added part to recieve json from outside in createEvent.php

--- createEvent.php
+++ createEvent.php
@@ -2,10 +2,11 @@
 /*script that will accept event info and send it to db */
 
     /*sample json for testing*/
-   $eventDataInJson='{ "title" : "skiing at Tahoe" , "description" : "Join us for skiing in Tahoe",
+   /*$eventDataInJson='{ "title" : "skiing at Tahoe" , "description" : "Join us for skiing in Tahoe",
                   "eventDate":"12-05-2015", "startTime":"0000-00-00 00:00:00","endTime":"0000-00-00 00:00:00",
-                  "ownerId":"1"}';
+                  "ownerId":"1"}';*/
 
+    $eventDataInJson = json_decode(file_get_contents("php://input"), true);
     $eventData=json_decode($eventDataInJson);
 
     /*grab values from json*/
@@ -28,7 +29,7 @@
         if(!$insertQueryResult){
             die("problem executing insert query");
         }
-        
+
         echo "Insert successful";
     }
 
